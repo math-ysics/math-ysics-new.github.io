0309b41f9d3c4c3e891e46ce7ed276a9797f84cc
math-ysics <concorde.air@gmail.com>
2025-02-20 19:17:58 -0800

    1

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -8,6 +8,15 @@ const Information = () => {
           <p className="leading-relaxed text-gray-200">
             I am a second-year undergraduate student in the Department of Computing + Mathematical Sciences at the California Institute of Technology. My research interests are in automated machine learning, language models, dimensionality reduction, and scientific applications of AI. I also enjoy statistical manifolds, computational physics, and theoretical astrophysics.
           </p>
+          <p className="leading-relaxed text-gray-200 mt-4">
+            Prior to the Institute, my research aimed to optimize tornadogenesis and parameter-space constructions (2019-2022), with particular focus on theoretical hodograph structures; I accumulated extensive experience with NWPs across spatiotemporal magnitudes, from microphysics to synoptic structures and teleconnections, spanning long-range GCMs to mesoscale rapid refresh models in both research and forecasting (2017-).
+          </p>
+          <p className="leading-relaxed text-gray-200 mt-4">
+            I also dabbled in some pre-college competitions (USA Team Selection Tests for International Science Olympiads, the Science Talent Search, the National Science Bowl National Finals, and the Science Olympiad National Tournament).
+          </p>
+          <p className="leading-relaxed text-gray-200 mt-4 italic">
+            One shall direct their correspondence to [first initial] + h + [last] + [at] + caltech + [dot] + edu.
+          </p>
         </div>
         <div className="w-3/5 p-4 flex items-center justify-center">
           <div className="relative w-full h-full">
