311fd8162f6f83b60d2b36ee357a9abec5405691
math-ysics <concorde.air@gmail.com>
2025-02-20 18:21:21 -0800

    1

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -4,7 +4,11 @@ const Information = () => {
   return (
     <div className="pt-16 flex flex-col h-screen">
       <div className="flex h-1/2 border-b border-white">
-        <div className="w-2/5 border-r border-white p-4">Top Left</div>
+        <div className="w-2/5 border-r border-white p-4">
+          <p className="leading-relaxed text-gray-200">
+            I am a second-year undergraduate student in the Department of Computing + Mathematical Sciences at the California Institute of Technology. My research interests are in automated machine learning, language models, dimensionality reduction, and scientific applications of AI. I also enjoy statistical manifolds, computational physics, and theoretical astrophysics.
+          </p>
+        </div>
         <div className="w-3/5 p-4 flex items-center justify-center">
           <div className="relative w-full h-full">
             <img 
