4e9c4c34b49eaec5c569a6d49829427756a94810
math-ysics <concorde.air@gmail.com>
2025-02-20 17:48:49 -0800

    1

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -5,10 +5,26 @@ const Information = () => {
     <div className="pt-16 flex flex-col h-screen">
       <div className="flex h-1/2 border-b border-white">
         <div className="w-2/5 border-r border-white p-4">Top Left</div>
-        <div className="w-3/5 p-4">Top Right</div>
+        <div className="w-3/5 p-4 flex items-center justify-center">
+          <div className="relative w-full h-full">
+            <img 
+              src="/images/nemophila-bg.jpg"
+              alt="Nemophila flowers background"
+              className="absolute inset-0 w-full h-full object-cover rounded-lg"
+            />
+          </div>
+        </div>
       </div>
       <div className="flex h-1/2">
-        <div className="w-3/5 border-r border-white p-4">Bottom Left</div>
+        <div className="w-3/5 border-r border-white p-4 flex items-center justify-center">
+          <div className="relative w-full h-full">
+            <img 
+              src="/images/sky.jpg"
+              alt="Sky background"
+              className="absolute inset-0 w-full h-full object-cover rounded-lg"
+            />
+          </div>
+        </div>
         <div className="w-2/5 p-4">Bottom Right</div>
       </div>
     </div>
